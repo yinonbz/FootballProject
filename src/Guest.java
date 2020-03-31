3fd822d83a80b31ea16251338e0ac876e9e96967
alongolo <alongolo@post.bgu.ac.il>
2020-03-31 17:12:22 +0300

    Alon

---
+++ src/Guest.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Guest {
+    private int guestId;
+
+
+    public Boolean register(){
+        return true;
+    }
+
+    public Boolean login(){
+        return true;
+    }
+
+    public List<Object> search(String searchInput){
+        return null;
+    }
+
+    public void openPage(){
+
+    }
+}
