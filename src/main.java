b3e57528fe289d423ea808dd33ed602b3d305c76
yinonbz <yinonbz@gmail.com>
2020-04-04 17:09:21 +0300

    initial

--- src/main.java
+++ src/main.java
@@ -1,7 +1,12 @@
 public class main {
     public static void main(String args[]){
         System.out.println("hello world");
+        Guest guest = new Guest();
+        Coach coach = new Coach("Arnon<3", "1234","Arnon","fitness coach","morale killer");
+        guest.login(coach);
 
 
     }
+
+
 }
