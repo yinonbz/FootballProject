bb31b0aa4c83a242a7dec4e7a72fb47776140e4b
alongolo <alongolo@post.bgu.ac.il>
2020-04-14 11:10:33 +0300

    Merge branches 'Alon' and 'Tomer' of https://github.com/yinonbz/FootballProject into Alon
     Conflicts:
    	Test/serviceLayer/TestSystemController.java
    	src/serviceLayer/SystemController.java

--- src/businessLayer/userTypes/viewers/Guest.java
+++ src/businessLayer/userTypes/viewers/Guest.java
@@ -17,14 +17,19 @@ public class Guest {
         this.systemController=systemController;
     }
 
-    /**
-     * @return
+    /** UC - 2.1
+     *   * This function will be called from the UI controller.
+     *   * It will start the registration procedure in the business layer.
+     * @returntrue if the registration was successful
+     *  false else
      */
     public Boolean register(){
+        if(systemController.createRegistrationForm(this) == null)
+            return false;
         return true;
     }
 
-    /**
+    /** UC - 2.2
      * This function will be called from the UI controller.
      * It will start the login procedure in the business layer.
      * @return true if the login was successful
@@ -80,11 +85,20 @@ public class Guest {
     }
 
     /**
-     * This function will mediate between the UI login form and the System controller. it will get the user's input from the GUI lofin form and will place the input in the parameters (String userNameInput, String passwordInput).
+     * This function will mediate between the UI login/registration form and the System controller. it will get the user's input from the GUI login/registration form and will place the input in the parameters (String userNameInput, String passwordInput).
      * @param userNameInput the userName input as entered at the GUI
      * @param passwordInput the password input as entered at the GUI
      */
     public void enterUserDetails(String userNameInput, String passwordInput) {
         //Send to UI to fill the userNameInput and passwordInput to create a new Subscriber with GUI
+    }
+
+    /**
+     * This function will mediate between the UI registration form and the System controller. it will get the user's input from the GUI registration form and will place the input in the parameter (String name)
+     * @param firstName the user's real first name input as entered at the GUI
+     * @param lastName the user's real first last input as entered at the GUI
+     */
+    public void enterUserRealName(String firstName, String lastName) {
+        //Send to UI to fill the user's real name to create a new Subscriber with GUI
     }
 }
