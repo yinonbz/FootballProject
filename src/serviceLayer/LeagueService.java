b120f514692e039deab4311ed3b1b848de927756
Itai Katz <itaikatz7@gmail.com>
2020-04-15 17:42:34 +0300

    LeagueService (Only Association Representative)

--- src/serviceLayer/LeagueService.java
+++ src/serviceLayer/LeagueService.java
@@ -3,6 +3,7 @@ package serviceLayer;
 import businessLayer.Tournament.League;
 import businessLayer.Tournament.LeagueController;
 import businessLayer.userTypes.Administration.AssociationRepresentative;
+import businessLayer.userTypes.Administration.Referee;
 import businessLayer.userTypes.Subscriber;
 import businessLayer.userTypes.SystemController;
 
@@ -35,11 +36,7 @@ public class LeagueService {
     public boolean addLeagueThroughRepresentative(String leagueID, String username) {
 
         if (leagueID != null && username != null) {
-            Subscriber user = systemController.getSubscriberByUserName(username);
-            if (user instanceof AssociationRepresentative) {
-                AssociationRepresentative userRep = (AssociationRepresentative) user;
-                return userRep.createLeague(leagueID);
-            }
+            return leagueController.addLeagueThroughRepresentative(leagueID, username);
         }
         return false;
     }
@@ -58,11 +55,7 @@ public class LeagueService {
     public boolean addSeasonThroughRepresentative(String leagueID, int seasonID, Date startingDate, Date endingDate, String username) {
 
         if (leagueID != null && username != null) {
-            Subscriber user = systemController.getSubscriberByUserName(username);
-            if (user instanceof AssociationRepresentative) {
-                AssociationRepresentative userRep = (AssociationRepresentative) user;
-                return userRep.createSeason(leagueID, seasonID, startingDate, endingDate);
-            }
+            return leagueController.addSeasonThroughRepresentative(leagueID, seasonID, startingDate, endingDate, username);
         }
         return false;
     }
@@ -78,11 +71,7 @@ public class LeagueService {
     public boolean createRefereeThroughRepresentative(String refUsername, String username) {
 
         if (refUsername != null && username != null) {
-            Subscriber user = systemController.getSubscriberByUserName(username);
-            if (user instanceof AssociationRepresentative) {
-                AssociationRepresentative userRep = (AssociationRepresentative) user;
-                return userRep.createReferee(refUsername);
-            }
+            return leagueController.createRefereeThroughRepresentative(refUsername, username);
         }
         return false;
     }
@@ -98,11 +87,7 @@ public class LeagueService {
     public boolean removeRefereeThroughRepresentative(String refUsername, String username) {
 
         if (refUsername != null && username != null) {
-            Subscriber user = systemController.getSubscriberByUserName(username);
-            if (user instanceof AssociationRepresentative) {
-                AssociationRepresentative userRep = (AssociationRepresentative) user;
-                return userRep.removeRefree(refUsername);
-            }
+            return leagueController.removeRefereeThroughRepresentative(refUsername, username);
         }
         return false;
     }
@@ -110,6 +95,7 @@ public class LeagueService {
 
     /**
      * The function receives a referee's username, leagueID, seasonID and the representative's username from the interface layer and calls the assigning function in the business layer
+     *
      * @param refUsername
      * @param leagueName
      * @param seasonID
@@ -119,12 +105,9 @@ public class LeagueService {
     public boolean assignRefereeThroughRepresentation(String refUsername, String leagueName, int seasonID, String username) {
 
         if (refUsername != null && leagueName != null && username != null) {
-            Subscriber user = systemController.getSubscriberByUserName(username);
-            if (user instanceof AssociationRepresentative) {
-                AssociationRepresentative userRep = (AssociationRepresentative) user;
-                return userRep.assignRefereeToSeason(refUsername, leagueName, seasonID);
-            }
+            return leagueController.assignRefereeThroughRepresentation(refUsername, leagueName, seasonID, username);
         }
         return false;
     }
+
 }
