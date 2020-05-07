bde8bbec9f3faf0bc94b91d74776456127c5681d
safamSaol <yosi_dekel@walla.com>
2020-05-07 20:05:44 +0300

    ido add the page

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,30 @@
 import businessLayer.Utilities.alertSystem.AlertSystem;
 import businessLayer.userTypes.SystemController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class main {
+import java.io.File;
+import java.net.URL;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        URL x =getClass().getClassLoader().getResource("sample.fxml");
+        Parent root = FXMLLoader.load(x);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    /*
     public static void main(String args[]){
         SystemController systemController = SystemController.SystemController();
         systemController.setAlertSystem(new AlertSystem());
@@ -9,8 +32,15 @@ public class main {
 
 
         int a = 2;
-
-
-
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+*/
 }
--- src/test/java/businessLayer/Utilities/PageTest.java
+++
@@ -1,4 +0,0 @@
-package businessLayer.Utilities;
-
-public class PageTest {
-}
