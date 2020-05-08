7242825768989466d26006cf8cd45d680d3ee000
safamSaol <yosi_dekel@walla.com>
2020-05-08 21:46:06 +0300

    no java fx

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,12 +9,42 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.net.URL;
 
-public class main {
+public class main extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //URL x =getClass().getClassLoader().getResource("sample.fxml");
+        URL x =getClass().getResource("sample.fxml");
+      //  FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
+       // Parent root = (Parent) fxmlLoader.load();
+
+        Parent root = FXMLLoader.load(x);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    /*
     public static void main(String args[]){
         SystemController systemController = SystemController.SystemController();
         systemController.setAlertSystem(new AlertSystem());
         SystemController systemController1 = SystemController.SystemController();
+
+
         int a = 2;
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+*/
 }
