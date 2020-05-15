cd1e6f6de7db635dbaa4b6c5145bc74a82b289b1
yinonbz <yinonbz@gmail.com>
2020-05-15 23:13:29 +0300

    Merge branch 'master-iteration2' of https://github.com/yinonbz/FootballProject into inon-iteration3
    # Conflicts:
    #	src/main/java/businessLayer/userTypes/Administration/Coach.java

--- src/main/java/dataLayer/DB_Inter.java
+++ src/main/java/dataLayer/DB_Inter.java
@@ -4,7 +4,7 @@ public interface DB_Inter {
 
     boolean containInDB(String objectName);
 
-    Object selectFromDB(String objectName,String table,String where,String groupby);
+    Object selectFromDB(String objectName);
 
     boolean removeFromDB(String objectName, String table,String where,String groupby);
 
