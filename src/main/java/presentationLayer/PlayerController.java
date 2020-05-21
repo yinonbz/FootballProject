2a834697a3b21cc2ba36485a60919fdac4611122
safamSaol <yosi_dekel@walla.com>
2020-05-21 18:02:11 +0300

    Merge branch 'Alon-FX-4' of https://github.com/yinonbz/FootballProject into idoFx
    # Conflicts:
    #	resources/fxml/AR.fxml
    #	src/main/java/presentationLayer/ARController.java
    #	src/main/java/presentationLayer/RefereeController.java
    #	target/classes/businessLayer/Tournament/Match/MatchController.class
    #	target/classes/fxml/AR.fxml
    #	target/classes/presentationLayer/ARController.class
    #	target/classes/presentationLayer/RefereeController.class

--- src/main/java/presentationLayer/PlayerController.java
+++ src/main/java/presentationLayer/PlayerController.java
@@ -1,8 +1,21 @@
 package presentationLayer;
 
+import businessLayer.userTypes.Administration.Player;
+import javafx.fxml.FXML;
+import serviceLayer.SystemService;
+
 public class PlayerController implements ControllerInterface{
+    SystemService service = new SystemService();
     @Override
     public void setUser(String usernameL) {
 
     }
+
+    @FXML
+    public void updatePage(){
+        String s = "updateTextArea";
+        //Player player = service.getPlayerUser(user);
+        //player.updatePage(s);
+    }
+
 }
