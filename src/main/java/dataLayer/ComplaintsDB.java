c5680aaad6d3396cf4d5f0fb1ba06170e04cb0b9
tomer <tomersein@gmail.com>
2020-05-23 22:40:50 +0300

    Merge branch 'master-iteration2' of https://github.com/yinonbz/FootballProject into tomerDataLayer
    # Conflicts:
    #	FootballProject.iml
    #	pom.xml
    #	src/main/java/businessLayer/Team/TeamController.java
    #	src/main/java/businessLayer/Tournament/League.java
    #	src/main/java/businessLayer/Tournament/LeagueController.java
    #	src/main/java/businessLayer/Tournament/Match/MatchController.java
    #	src/main/java/businessLayer/Tournament/Season.java
    #	src/main/java/businessLayer/Utilities/Page.java
    #	src/main/java/businessLayer/userTypes/Administration/Coach.java
    #	src/main/java/businessLayer/userTypes/Administration/TeamOwner.java
    #	src/main/java/businessLayer/userTypes/SystemController.java
    #	src/main/java/serviceLayer/SystemService.java
    #	src/test/java/businessLayer/userTypes/Administration/AssociationRepresentativeTest.java

--- src/main/java/dataLayer/ComplaintsDB.java
+++ src/main/java/dataLayer/ComplaintsDB.java
@@ -25,7 +25,7 @@ public class ComplaintsDB implements DB_Inter{
 
     public ComplaintsDB(){
         this("root","Messi1Ronaldo2",
-                "org.mariadb.jdbc.Driver","jdbc:mysql://localhost:3306/demodb");
+                "org.mariadb.jdbc.Driver","jdbc:mysql://132.72.65.33:3306/demodb");
     }
 
     public ComplaintsDB(String username,String password, String myDriver, String myUrl){
